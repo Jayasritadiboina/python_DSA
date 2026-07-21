1773c3e9d6c2bac1d8334d55b50de851428ec6ad
jayasri <jayasritadiboinajayasri@gmail.com>
2026-07-21 11:04:42 +0530

    push tp github

---
+++ main.py
@@ -0,0 +1,34 @@
+'''for i in range(5):
+    i="jayasri"
+    print(i)'''
+'''print("sriram\nsriram1")
+print("\tcurrent\new\folder")'''
+#eve or odd
+'''def check(num):
+    if num%2 == 0:
+        print("even")
+    else:
+        print("odd")
+    check(20)'''
+#create a empty dict
+'''dict={
+    "name":"ram",
+    "gender":"male",
+    "age":20,
+    "courses":["python","java","datascience"]
+}
+dict.update(["name"])="sairam"'''
+
+#print it back
+'''x="rara"
+x=x[::-1]
+print(x)
+
+while True:
+    print("hi")'''
+#take a num as input and find the sum of num from 1 to that number
+x=int(input("enter number:"))
+sum =0
+for i in range(sum,x+1):
+    sum += i
+    print("sum=",sum)
